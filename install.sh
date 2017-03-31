f49935acf73277f904c35075de6e7703979861ab
Jan Clarin <hi@janclarin.com>
2017-03-31 11:12:01 -0600

    Only try to create dotfiles.old once

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ echo "Pull and update submodules:"
 git submodule update --init --recursive
 
 # Symlink dotfiles and zim folder.
-echo "Symlink and back up dotfiles:"
+echo "Symlink and back up dotfiles in dotfiles.old:"
+mkdir -pv dotfiles.old
 for file in aliases bashrc dotfiles_profile exports gitconfig gitignore path \
     zim zlogin zshrc; do
-    mkdir -pv dotfiles.old
     [ -e ~/.$file ] && mv -v ~/.$file dotfiles.old/.$file
     ln -sfv $SETUP_DIR/$file ~/.$file
 done
