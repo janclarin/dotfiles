5607611b0b98e87ca93703a6f9fe2c5e7d7ee2c2
Jan Clarin <hi@janclarin.com>
2017-04-22 15:03:24 -0600

    Add install-extra script

---
+++ install-extra.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+# Install extra npm packages.
+if (( $+commands[npm] )); then
+    # tiny-care-terminal
+    _install_tiny_care_terminal
+fi
+
+_install_tiny_care_terminal() {
+    npm install -g git-standup tiny-care-terminal
+
+    local exportsLocalFile='~/.exports.local'
+    if [ ! -e "$exportsLocalFile" ]; then
+        touch "$exportsLocalFile"
+    fi
+
+    echo "Config for tiny-care-terminal should be set in ~/.exports.local"
+    echo "For more info: github.com/notwaldorf/tiny-care-terminal"
+}
