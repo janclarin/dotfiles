8ebe6ef30cd79d0b81479195ee00c668fa90940e
Jan Clarin <hi@janclarin.com>
2017-09-26 10:35:28 -0700

    Tmux: remove tpm

--- install-tmux.sh
+++ install-tmux.sh
@@ -7,8 +7,5 @@ SETUP_DIR=$(pwd)
 # Back up any existing .tmux.conf as .tmux.conf.old.
 mv ~/.tmux.conf ~/.tmux.conf.old 2> /dev/null
 
-# Install tpm.
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
 # Link the tmux.conf file to ~/.tmux.conf
 ln -sf $SETUP_DIR/tmux.conf ~/.tmux.conf
