dd5d20659324a4fd5659c8566abb619bce558323
Jan Clarin <me@janclarin.com>
2020-12-14 23:01:46 -0800

    Vim: add neovim installation support on debian

---
+++ init.vim
@@ -0,0 +1,7 @@
+" =============================================================================
+" Jan Clarin's neovim configuration file
+" =============================================================================
+
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc
--- install-vim.sh
+++ install-vim.sh
@@ -26,16 +26,13 @@ if [ "$1" = "--nvim" ]; then
         brew install neovim
         pip2 install --user neovim
         pip3 install --user neovim
-
-        # Link vim configuration to neovim.
-        mkdir -p ~/.config/nvim/autoload
-        ln -sf $SETUP_DIR/vimrc ~/.config/nvim/init.vim
-        ln -sf ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/
     else
-        echo "Neovim installation is currently only supported on macOS"
-        echo "To install Neovim manually on other operating systems, refer to github.com/neovim/neovim/wiki/Installing-Neovim"
-        echo "After installing, run the following: mkdir ~/.config; ln -s ~/.vim ~/.config/nvim; ln -s ~/.vimrc ~/.config/nvim/init.vim"
+        sudo apt-get install neovim python-neovim python3-neovim
     fi
+
+    # Link vim configuration to neovim.
+    mkdir -p ~/.config/nvim/autoload
+    ln -sf $SETUP_DIR/init.vim ~/.config/nvim/init.vim
 elif [ "$1" = "--ideavim" ]; then
     ln -sf $SETUP_DIR/ideavimrc ~/.ideavimrc
     echo "ideavimrc has been linked as ~/.ideavimrc and installed plugins."
